c52354279637b29c2af62bb854d52414685aa0fe
naveen prasanth <naveenprasanth12@gmail.com>
2024-05-18 19:53:50 +0530

    initial push

---
+++ src/test/java/com/bharath/springdata/componentmapping/ComponentMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.springdata.componentmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
